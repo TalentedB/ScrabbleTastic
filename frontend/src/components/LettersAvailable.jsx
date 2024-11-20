6231ba1e32ed242e3348a44d3e4d31b810793cee
Sebastien <sebastienlegerperre@gmail.com>
2024-11-20 07:02:37 -0800

    added cool bg color for available letters

--- frontend/src/components/LettersAvailable.jsx
+++ frontend/src/components/LettersAvailable.jsx
@@ -1,8 +1,8 @@
 export const LettersAvailable = ({ lettersAvailable }) => {
   return (
-    <div className="flex justify-around w-1/2 content-center mx-auto mt-5">
+    <div className="flex justify-around w-1/2 content-center mx-auto mt-5 bg-amber-900 p-1">
       {lettersAvailable.map((letter) => (
-        <div className="border border-black w-8 h-8 text-center text-base">
+        <div className="border bg-amber-200 border-black w-8 h-8 text-center text-base">
           {letter}
         </div>
       ))}
