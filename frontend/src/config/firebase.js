cb4a7b607296dc2957146d9cadae7271db26c750
Sebastien <sebastienlegerperre@gmail.com>
2025-01-11 00:34:37 -0800

    Generated new API Key for firebase and saved it to a .env file

--- frontend/src/config/firebase.js
+++ frontend/src/config/firebase.js
@@ -8,15 +8,14 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA-zUlXc1lZ1P3lo716rB72-dvlbg7PNto",
-  authDomain: "scrabbletastic-6f7a1.firebaseapp.com",
-  projectId: "scrabbletastic-6f7a1",
-  storageBucket: "scrabbletastic-6f7a1.firebasestorage.app",
-  messagingSenderId: "948579694152",
-  appId: "1:948579694152:web:589444214eebf95793ca7d",
-  measurementId: "G-6Q9PFGM3VM",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
